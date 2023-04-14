08fc9f55d830132730e65ff63e903b742bf55e3f
willianctti <willianctti@gmail.com>
2023-04-14 17:06:47 -0300

    .

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,17 +7,19 @@ import { HomeComponent } from './home/home.component';
 // Modules
 import { RoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
